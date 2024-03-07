f8d8105791480e2380646b982fb8e5bb5521839a
Sharan <jagsharan2587@gmail.com>
2024-03-08 01:33:42 +0530

    01-Create-Next-App-Layout

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import Header from '@/components/header/header'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Amazona-next-app',
+  description: 'modern Ecommerce website',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          {children}
+          <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <p>Copyright 2023 - All right reserved by Next Amazona V2</p>
+          </footer>
+        </div>
+      </body>
+    </html>
+  )
+}
